dd7232bc7d3b9654937e3c1fe5b812970fdc7685
Bert <bertmulder@gmail.com>
2025-09-24 00:42:07 +0200

    vitteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,50 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react', 'googleapis'],
+//   },
+//   define: {
+//     global: 'globalThis',
+//     'process.env': '{}',
+//   },
+//   resolve: {
+//     alias: {
+//       process: 'process/browser',
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['googleapis'],
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react', 'googleapis', '@aws-sdk/client-s3'],
+  },
+  define: {
+    global: 'globalThis',
+    'process.env': '{}',
+  },
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      './runtimeConfig': './runtimeConfig.browser', // important for aws-sdk
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['googleapis', '@aws-sdk/client-s3'],
+    },
   },
 });
